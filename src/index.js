a7d38430382f6c93597795606432a996464df851
Ishari <mastoffp@gmail.com>
2019-10-25 21:13:01 +0200

    Style base

--- src/index.js
+++ src/index.js
@@ -93,16 +93,17 @@ class Game extends React.Component {
 
     return (
       <div className="game">
+        <div className="game-status">{status}</div>
         <div className="game-board">
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
+        <ol className="game-moves">
+          <li>Move list</li>
+          <li>Here</li>
+        </ol>
       </div>
     );
   }
